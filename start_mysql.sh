08715e4b7316a1495c33a6019da73cfe52938dbb
RLDevOps <sandeep.ranganath@relevancelab.com>
2014-06-24 20:31:08 +0530

    Update start_mysql.sh

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 service mysqld start
-/usr/bin/mysql -uroot -proot123 catalog < /opt/Catalog_DBscripts/jacatlog_ddl.sql 
-/usr/bin/mysql -uroot -proot123 catalog < /opt/Catalog_DBscripts/jacatlog_dml.sql
+/usr/bin/mysql -uroot -proot123 catalog < /opt/jacatlog_ddl.sql 
+/usr/bin/mysql -uroot -proot123 catalog < /opt/jacatlog_dml.sql
 /usr/bin/mysqld_safe
